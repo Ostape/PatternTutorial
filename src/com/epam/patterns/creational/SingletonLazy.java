87cd612fecda69f5523a5d069ddbbffc1ddc4ca7
Valeriia Fatova <valeriiafatova@Air-Valeriia.kyiv.epam.com>
2019-04-01 15:03:21 +0300

    01 singleton simple

---
+++ src/com/epam/patterns/creational/SingletonLazy.java
@@ -0,0 +1,18 @@
+package com.epam.patterns.creational;
+
+public class SingletonLazy {
+    private static SingletonLazy instance = null;
+    private SingletonLazy(){
+
+    }
+    public static SingletonLazy getInstance(){
+        if(instance == null){
+            instance = new SingletonLazy();
+        }
+        return instance;
+    }
+
+    public void setUp(){
+        System.out.println("I am in");
+    }
+}
