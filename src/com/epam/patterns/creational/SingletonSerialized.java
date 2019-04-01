407fe13c3352da0ef12ef7ccb2612b354f55aec5
Valeriia Fatova <valeriiafatova@Air-Valeriia.kyiv.epam.com>
2019-04-01 15:43:08 +0300

    04 Singleton serializable

---
+++ src/com/epam/patterns/creational/SingletonSerialized.java
@@ -0,0 +1,47 @@
+package com.epam.patterns.creational;
+
+import java.io.*;
+
+public class SingletonSerialized implements Serializable {
+
+    public static final long serialVersionUID = -8610417477740383133L;
+
+    private SingletonSerialized(){
+
+    }
+
+    private static class SingletonHelper{
+        private static final SingletonSerialized INSTANCE = new SingletonSerialized();
+    }
+
+    public static SingletonSerialized getInstance(){
+        return SingletonHelper.INSTANCE;
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        SingletonSerialized instanceOne = SingletonSerialized.getInstance();
+        ObjectOutput out = new ObjectOutputStream(new FileOutputStream("testSingleton.ser"));
+        out.writeObject(instanceOne);
+        out.close();
+
+        ObjectInput in = new ObjectInputStream(new FileInputStream("testSingleton.ser"));
+        SingletonSerialized instanceTwo = (SingletonSerialized) in.readObject();
+        in.close();
+
+        System.out.println("instanceOne hash: " + instanceOne.hashCode());
+        System.out.println("instanceTwo hash: " + instanceTwo.hashCode());
+    }
+
+
+
+
+
+
+
+
+
+
+//    protected Object readResolve(){
+//        return getInstance();
+//    }
+}
